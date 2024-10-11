1224846cd425122795a4a11af44abeaa65258426
Timo <feldman.timo@gmail.com>
2024-10-11 18:54:23 -0500

    Added concrete conversion functionality

---
+++ src/main/java/net/steakboi/betterconcreteconversion/BetterConcreteConversion.java
@@ -0,0 +1,12 @@
+package net.steakboi.betterconcreteconversion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterConcreteConversion implements ModInitializer {
+	public static final String MOD_ID = "betterconcreteconversion";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
